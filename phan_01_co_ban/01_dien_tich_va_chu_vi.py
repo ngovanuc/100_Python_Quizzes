6bb032c582d20a6fef91fefe64e137dbf30afe31
ngovanuc <ngovanuc.1508@gmail.com>
2025-05-07 08:07:54 +0700

    commit

---
+++ phan_01_co_ban/01_dien_tich_va_chu_vi.py
@@ -0,0 +1,17 @@
+"""
+Bài toán: Tính diện tích và chu vi hình vuông
+
+"""
+
+# Nhập chiều dài cạnh a
+a = float(input("Nhập chiều dài cạnh a: "))
+
+# Tính diện tích
+S = a ** 2
+
+# Tính chu vi
+C = 4 * a
+
+# In kết quả
+print("Diện tích hình vuông là:", S)
+print("Chu vi hình vuông là:", C)
